e0277e85471a2134b30f9ea287066c6a1c3a7fe3
zhongfuqiang <liangyubang@gmail.com>
2017-07-20 14:29:52 +0800

    Initial commit

---
+++ app/src/main/java/cn/fiona/sharesdk/MainActivity.java
@@ -0,0 +1,12 @@
+package cn.fiona.sharesdk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
